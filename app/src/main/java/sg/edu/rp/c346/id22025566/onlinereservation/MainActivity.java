c412f6a51c723cf4aa9232e7cfa6da63bd5ca3c6
22025566 <22025566@myrp.edu.sg>
2023-05-11 21:37:54 +0800

    Online Reservation

--- app/src/main/java/sg/edu/rp/c346/id22025566/onlinereservation/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22025566/onlinereservation/MainActivity.java
@@ -3,11 +3,14 @@ package sg.edu.rp.c346.id22025566.onlinereservation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
@@ -23,10 +26,12 @@ public class MainActivity extends AppCompatActivity {
     EditText etSizeGrp;
     DatePicker dp;
     TimePicker tp;
-    CheckBox cbSmoking;
-    CheckBox cbNonSmoking;
+    RadioGroup rgSmoke;
+    RadioButton rbSmoking;
+    RadioButton rbNonSmoking;
     Button btnConfirm;
     Button btnReset;
+    TextView tvDisplayToast;
 
 
 
@@ -43,32 +48,93 @@ public class MainActivity extends AppCompatActivity {
         etSizeGrp=findViewById(R.id.editTextSizeGrp);
         dp=findViewById(R.id.datePicker);
         tp=findViewById(R.id.timePicker);
-        cbSmoking=findViewById(R.id.checkBoxSmoking);
-        cbNonSmoking=findViewById(R.id.checkBoxNonSmoking);
+        rgSmoke=findViewById(R.id.radioGroupSmoke);
+        rbSmoking=findViewById(R.id.radioButtonSmoking);
+        rbNonSmoking=findViewById(R.id.radioButtonNonSmoking);
         btnConfirm=findViewById(R.id.buttonConfirm);
         btnReset=findViewById(R.id.buttonReset);
+
+
+
+        int defaultHour = 19;
+        int defaultMinute = 30;
+        tp.setHour(defaultHour);
+        tp.setMinute(defaultMinute);
+
+        // set to default date
+        int defaultYear = 2023;
+        int defaultMonth = 5;
+        int defaultDayOfMonth = 1;
+        dp.updateDate(defaultYear, defaultMonth, defaultDayOfMonth);
 
         //What happens when CONFIRM button is clicked
         btnConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                String name=etName.getText().toString();
-                String mobile=etMobile.getText().toString();
+                String name = etName.getText().toString();
+                String mobile = etMobile.getText().toString();
+                String size = etSizeGrp.getText().toString();
+
+                //Retrieve date
+                int getYear = dp.getYear();
+                int getMonth = dp.getMonth() + 1;
+                int getDayOfMonth = dp.getDayOfMonth();
+                String date = getDayOfMonth + "/" + getMonth + "/" + getYear;
+
+                //Retrieve time
+                String stringAnswer = "";
+                int getHour = tp.getHour();
+                int getMinute = tp.getMinute();
+                stringAnswer = getHour + ":" + getMinute;
+
+                //Retrieve state of radioButton ie. which is checked
+                String stringSmoke = "Smoking area";
+                String stringNoSmoke = "Non-smoking area";
+                String stringResponse = "";
+
+                int checkedRadioId = rgSmoke.getCheckedRadioButtonId();
+
+                if (checkedRadioId==R.id.radioButtonSmoking) {
+                    stringResponse="Smoking area";
+                }
+                else{
+                    stringResponse="Non-smoking area";
+                }
 
 
-                Toast.makeText(MainActivity.this, "Name: " + name, Toast.LENGTH_LONG).show();
-                Toast.makeText(MainActivity.this, "Mobile: " + mobile, Toast.LENGTH_LONG).show();
+
+                Toast.makeText(MainActivity.this, "Name: " + name, Toast.LENGTH_SHORT).show();
 
 
-
-
-
-
+                Toast.makeText(MainActivity.this, "Mobile: " + mobile, Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Group size: " + size, Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Date: " + date +  "  Time: " + stringAnswer , Toast.LENGTH_SHORT).show();
+                //Toast.makeText(MainActivity.this, "Time: " + stringAnswer, Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Area: " + stringResponse, Toast.LENGTH_SHORT).show();
 
 
             }
         });
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int defaultYear = 2023;
+                int defaultMonth = 5;
+                int defaultDayOfMonth = 1;
+                dp.updateDate(defaultYear, defaultMonth, defaultDayOfMonth);
+
+
+                int defaultHour = 19;
+                int defaultMinute = 30;
+                tp.setHour(defaultHour);
+                tp.setMinute(defaultMinute);
+
+
+            }
+        });
+
 
 
 
